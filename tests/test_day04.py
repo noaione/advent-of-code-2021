1ae73b36d53490495688503480bd7afd51dcbc13
noaione <noaione0809@gmail.com>
2021-12-04 14:30:14 +0800

    [tests] add more day 4 test

--- tests/test_day04.py
+++ tests/test_day04.py
@@ -25,16 +25,51 @@ EXAMPLES = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,
 10 16 15  9 19
 18  8 23 26 20
 22 11 13  6  5
- 2  0 12  3  7"""
+ 2  0 12  3  7""".split("\n")
+
+
+def test_board_parse():
+    boards, _ = parse_board(EXAMPLES)
+    assert len(boards) == 3
+
+
+def test_play_order_parse():
+    actual_order = list(map(int, EXAMPLES[0].split(",")))
+    _, play_order = parse_board(EXAMPLES)
+    assert play_order == actual_order
+
+
+def test_board_play():
+    boards, _ = parse_board(EXAMPLES)
+    boards[0].play(23)
+    assert boards[0].marker[1][2] is True
+
+
+def test_board_check():
+    boards, _ = parse_board(EXAMPLES)
+    boards[0].play(23)
+    assert boards[0].check() is False
+    boards[0].play(24)
+    boards[0].play(4)
+    boards[0].play(8)
+    boards[0].play(2)
+    assert boards[0].check() is True
+    assert boards[0]._check_col() is False
+    assert boards[0]._check_row() is True
+
+
+def test_board_sum_unmarked():
+    boards, _ = parse_board(EXAMPLES)
+    boards[0].play(23)
+    boards[0].play(24)
+    assert boards[0].sum_of_unmarked() == 253
 
 
 def test_part_a_solution():
-    examples_in = [num.rstrip() for num in EXAMPLES.split("\n")]
-    boards, game_order = parse_board(examples_in)
+    boards, game_order = parse_board(EXAMPLES)
     assert part_a(boards, game_order) == 4512
 
 
 def test_part_b_solution():
-    examples_in = [num.rstrip() for num in EXAMPLES.split("\n")]
-    boards, game_order = parse_board(examples_in)
+    boards, game_order = parse_board(EXAMPLES)
     assert part_b(boards, game_order) == 1924
