3dfeae0cf190a6dfec1a13de7fc3cce64d7c1d9e
noaione <noaione0809@gmail.com>
2021-12-07 14:01:14 +0800

    Faster solution with math

--- src/day07/solution.py
+++ src/day07/solution.py
@@ -17,18 +17,6 @@ def part_a(input_string: str) -> int:
     return min(possibilites.values())
 
 
-def step_number(number: int) -> int:
-    # Example:
-    # If we got the number 0 means we just need to return zero
-    # If we got one, we return one since it only one move
-    # If we got maybe three, we make it one + two + three which equals six
-
-    final = 0
-    for i in range(1, number + 1):
-        final += i
-    return final
-
-
 # Part B
 def part_b(input_string: str) -> int:
     crabs = list(map(int, input_string.split(",")))
@@ -41,13 +29,12 @@ def part_b(input_string: str) -> int:
     for i in possibilites:
         for j in crabs:
             move = abs(j - i)
-            possibilites[i] += step_number(move)
+            possibilites[i] += move * (move + 1) // 2
     return min(possibilites.values())
 
 
 if __name__ == "__main__":
     in_data = [num.rstrip() for num in open("input", "r").readlines() if num][0]
-    examples = "16,1,2,0,4,2,7,1,2,14"
 
     print(f"Part A: {part_a(in_data)}")
     print(f"Part B: {part_b(in_data)}")
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -5,7 +5,7 @@ current_path = Path(__file__).absolute().parent.parent
 source_dir = current_path / "src"
 sys.path.insert(0, str(source_dir))
 
-from day07.solution import part_a, part_b, step_number  # noqa
+from day07.solution import part_a, part_b  # noqa
 
 EXAMPLES = "16,1,2,0,4,2,7,1,2,14"
 
@@ -16,10 +16,3 @@ def test_part_a_solution():
 
 def test_part_b_solution():
     assert part_b(EXAMPLES) == 168
-
-
-def test_number_stepping():
-    assert step_number(0) == 0
-    assert step_number(1) == 1
-    assert step_number(2) == 3
-    assert step_number(3) == 6
